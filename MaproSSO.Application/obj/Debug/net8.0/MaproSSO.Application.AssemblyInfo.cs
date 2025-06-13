3e87978d0402d897abea66a7f7bd8a7aa2c07ab2
Marco Pajuelo <mpajuelo@maprosoft.com.pe>
2025-06-12 23:37:01 -0500

    ok

--- MaproSSO.Application/obj/Debug/net8.0/MaproSSO.Application.AssemblyInfo.cs
+++ MaproSSO.Application/obj/Debug/net8.0/MaproSSO.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaproSSO.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e8be71ee14b878749a668d4bd5cd94877653ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e250be28bbe1bde60d7399027e7500c01a719e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaproSSO.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaproSSO.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
